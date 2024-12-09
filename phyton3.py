be64f00c389aa5397fd80847e9fcf66491732388
Tu Nombre <you@example.com>
2024-12-09 11:41:17 +0100

    IES9D

---
+++ phyton3.py
@@ -0,0 +1,9 @@
+
+nombre = input(" Dame tu nombre: ") 
+print ("hola, ", nombre)
+
+n1 = int(input("dame un número "))
+n2 = int(input("dame otro número "))
+
+print("su suma es", n1+n2)
+print("su multiplicación es", n1*n2)
