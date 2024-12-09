3dac4e6d76f4e96731eb8183a30d82c57fc9fa1d
Tu Nombre <you@example.com>
2024-12-09 11:58:59 +0100

    IES9D

---
+++ phyton3_3.py
@@ -0,0 +1,6 @@
+n1 = int(input("dame un número "))
+n2 = int(input("dame otro número "))
+n3 = int(input("dame un último número"))
+
+print("el resultado de la suma de los dos primeros números multiplicado por el tercero es: ", (n1+n2)*n3)
+print("el resultado de la suma de las multiplicaciones del primero por el tercero y la multiplicación del segundo por el tercero es : ", (n1*n3)+(n2*n3))
