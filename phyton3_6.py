77c512a50d471268aa1f1cd089613896d472229d
Tu Nombre <you@example.com>
2024-12-10 09:51:00 +0100

    IES10D

--- phyton3_6.py
+++ phyton3_6.py
@@ -1,2 +1,3 @@
-n1 = int(input("dame un número real :"))
-n2 = int(input("dame otro número real : ")) 
+n1 = int(input("dame un número real : "))
+n2 = int(input("dame otro número real : "))
+print("la división de dichos números sin decimales es: ", n1//n2)
