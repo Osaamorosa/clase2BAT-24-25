278b23816a0287a269d2d9bb927c4446edb478d9
Tu Nombre <you@example.com>
2024-12-11 13:46:48 +0100

    IES11D

---
+++ phyton4_3.py
@@ -0,0 +1,7 @@
+n1 = int(input("dame un número entero : "))
+n2 = int(input("dame otro número entero : "))
+
+if n2 == 0 :
+   print("no se puede dividir entre 0")
+else:
+    print("el resultado de la división es:", n1/n2)
