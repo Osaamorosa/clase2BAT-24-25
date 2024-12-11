54d9de5a627e8b600e0014070f9c37e091ce2a54
Tu Nombre <you@example.com>
2024-12-11 13:40:20 +0100

    IES11D

---
+++ phyton4_2.py
@@ -0,0 +1,10 @@
+n1 = int(input("dime un número real: "))
+
+if n1 > 0: 
+    print ("el número es positivo")
+
+else: 
+    if n1 < 0:
+        print("el número es negativo")
+    else: 
+        print("el número es 0")
