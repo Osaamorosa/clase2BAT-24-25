23c11e040cc574ce9c3977c1ce0c5a8a77437e3d
Tu Nombre <you@example.com>
2025-01-08 13:39:26 +0100

    IES8E

---
+++ phyton5_2.py
@@ -0,0 +1,26 @@
+n = int(input("dame un número del 1 al 12: "))
+
+if n == 1 :
+    print("enero")
+elif n == 2 :
+    print("febrero")
+elif n == 3 :
+    print("marzo")
+elif n == 4 :
+    print("abril")
+elif n == 5 :
+    print("mayo")
+elif n == 6 :
+    print("junio")
+elif n == 7 :
+    print("julio")
+elif n == 8 :
+    print("agosto")
+elif n == 9 :
+    print("septiembre")
+elif n == 10 :
+    print("octubre")
+elif n == 11 :
+    print("noviembre")
+elif n == 12 :
+    print("diciembre")
