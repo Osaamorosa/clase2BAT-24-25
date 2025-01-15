d13647b26cd783fede49be347b91522f4dd14b0b
Tu Nombre <you@example.com>
2025-01-15 13:46:45 +0100

    IES15E

---
+++ phyton6_2.py
@@ -0,0 +1,13 @@
+#Elaborar una clave y una contraseña segura
+clave = int(input("dame un código que contenga 5 palabras: "))
+contraseña = int(input("dame una clave con 4 números"))
+
+while (clave != admin) : 
+    clave(input("código no válido"))
+print("código aceptado")
+
+while (contraseña != "0987") : 
+    contraseña(input("contraseña no válida"))
+print("contraseña aceptada")
+
+
