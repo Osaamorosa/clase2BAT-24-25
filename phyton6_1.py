12309538bf655ed26595d1f275808b8524366a37
Tu Nombre <you@example.com>
2025-01-15 13:27:25 +0100

    IES15E

---
+++ phyton6_1.py
@@ -0,0 +1,8 @@
+#suma de dos números
+resultado = int(input("¿cuál es el resultado de la suma de 135 + 768: "))
+
+while (resultado != 903) :
+    resultado = (135 + 768)
+    resultado = float(input("vuelve a intentarlo: "))
+print("correcto")
+    
