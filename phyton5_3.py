a6e7c0b0a8b81b84c05780572927e6593445cadb
Tu Nombre <you@example.com>
2025-01-21 09:36:43 +0100

    IES21E

--- phyton5_3.py
+++ phyton5_3.py
@@ -1,3 +1,10 @@
- Escribe un programa que pida un año y que escriba si es bisiesto o no.
-Se recuerda que los años bisiestos son múltiplos de 4, pero los múltiplos de 100
-no lo son, aunque los múltiplos de 400 sí
+#Escribe un programa que pida un año y que escriba si es bisiesto o no.
+#Se recuerda que los años bisiestos son múltiplos de 4, pero los múltiplos de 100
+#no lo son, aunque los múltiplos de 400 sí
+
+año = int(input("dame un año y dime si es bisiesto o no: "))
+
+if año%4 == 0 or año%400 == 0 :
+    print("es bisiesto")
+elif año%100 != 0 : 
+    print("no es bisiesto")
