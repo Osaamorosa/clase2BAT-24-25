09e4f592f6c0d64787d1efe6a8e33de7e9bb29b2
Tu Nombre <you@example.com>
2025-01-22 13:23:03 +0100

    IES22E

--- phyton6_3.py
+++ phyton6_3.py
@@ -1,6 +1,11 @@
 #Escriba un programa que pregunte una y otra vez si desea continuar con el
 #programa, siempre que se conteste exactamente sí (en minúsculas y con tilde)
 
-clave = input("¿Desea continuar con el programa?")
+clave = input("¿Desea continuar con el programa?: ")
 
-while clave != "sí"
+while (clave == "sí") :
+    clave = input("¿Desea continuar con el programa?: ")
+print("Como ha optado por no continuar, chao pescao :)")
+
+    
+
