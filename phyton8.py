6dd16267345696805f7e55963bef59bf9a6074b5
Tu Nombre <you@example.com>
2025-01-29 13:48:12 +0100

    IES29E

---
+++ phyton8.py
@@ -0,0 +1,14 @@
+#declaro el vector datos
+datos = []
+
+
+for i in range (0,5) :
+    datos.append(int(input("dame un dato: ")))
+
+datos[0] = 500
+for i in range (0,5) :
+    print(datos[i])
+
+
+
+
