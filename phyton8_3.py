626ef4d9fb2dc3ef929f393a276ff7ea8872e2bc
Tu Nombre <you@example.com>
2025-02-03 12:02:56 +0100

    IES3F

---
+++ phyton8_3.py
@@ -0,0 +1,12 @@
+#Pide al usuario un número del 1 al 12 y escribe el número del mes
+#correspondiente (1 = enero, 2= febrero,..) usando un array
+
+meses = ["enero", "febrero", "marzo", "abirl", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+
+n = int(input("dame un número del 1 al 12: "))
+
+if 1 <= n <= 12 : 
+    print("el mes correspondiente al número dado es: " meses[n - 1])
+
+else: 
+    print("número no válido, introduzca otra número entre el 1 y el 12: ")
